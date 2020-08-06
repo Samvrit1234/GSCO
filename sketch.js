8054f19cdaba7b2d691700d98c867f9912566d54
Samvrit1234 <68464137+Samvrit1234@users.noreply.github.com>
2020-08-06 11:09:31 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -11,13 +11,7 @@ function setup() {
 
   speed = random(55.90);
   weight = random(400,1500)
-  
-}
-
-function draw() {
-createCanvas(1600,400)
-    background(0,0,0);
-    if (car.x - wall.x <(car.width + wall.width)/2)
+   if (car.x - wall.x <(car.width + wall.width)/2)
     {
       car.velocityX = 0;
       var deformation = 0.5 * weight * speed * speed/22509;
@@ -33,6 +27,13 @@ createCanvas(1600,400)
                 car.shapeColor = color(0,255,0);
               }
          }
+  
+}
+
+function draw() {
+createCanvas(1600,400)
+    background(0,0,0);
+   
       }
     }
   }
